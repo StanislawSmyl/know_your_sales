d271e9d0f20c411dfd8f058cc53c16fd25275605
StanislawSmyl <39587667+StanislawSmyl@users.noreply.github.com>
2018-07-19 21:39:37 +0200

    static files
    css/fonts/js

---
+++ static/js/jsSearch.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	x = {{ ! products }};
+	console.log(x);
+	console.log(JSON.parse(x));
+	var prodList = $('p').html(), prodDate;
+	prodList = JSON.parse(prodList);
+	console.log(prodList);
+	var prodDate = Object.values(prodList);
+	prodDate = prodDate.filter( function(x) { return x != null });
+	console.log(prodDate);	
+	$(".searchForm").select2({
+		placeholder: "Select product",
+		data: prodDate
+	});
+});
